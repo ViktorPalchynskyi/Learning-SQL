08bb1fcfa9f6cfb4a017a91f6c149d27075d3c89
Viktor <palchinskij19@gmail.com>
2024-09-07 21:04:24 +0300

    07.09.24 - dropping and using dbs;

--- mySQL/Creating_Databases_and_Tables/index.sql
+++ mySQL/Creating_Databases_and_Tables/index.sql
@@ -1,2 +1,18 @@
 SHOW DATABASES;
+
+-- To create a db;
+
+CREATE DATABASE <name>;
 CREATE DATABASE SlimeStore;
+
+-- To drop a database:
+
+DROP DATABASE <database-name>;
+
+-- To use a database:
+
+USE <database-name>;
+
+-- To check current db;
+
+SELECT database();
