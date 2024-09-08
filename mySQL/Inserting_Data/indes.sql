d91be3f33e61fb7a4c4381f2c8a13530fd42801a
Viktor <palchinskij19@gmail.com>
2024-09-08 20:19:11 +0300

    08.09.24 - primary keys

--- mySQL/Inserting_Data/indes.sql
+++ mySQL/Inserting_Data/indes.sql
@@ -46,3 +46,20 @@ CREATE TABLE cats4  (
     name VARCHAR(20) NOT NULL DEFAULT 'unnamed',    
     age INT NOT NULL DEFAULT 99 
 );
+
+-- One way of specifying a PRIMARY KEY
+
+CREATE TABLE unique_cats (
+	cat_id INT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    age INT NOT NULL
+);
+
+-- Another option:
+
+CREATE TABLE unique_cats2 (
+	cat_id INT,
+    name VARCHAR(100) NOT NULL,
+    age INT NOT NULL,
+    PRIMARY KEY (cat_id)
+);
