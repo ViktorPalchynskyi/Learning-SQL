e794f088042f1804e87c8736658830485275c74a
Viktor <palchinskij19@gmail.com>
2024-09-08 19:25:41 +0300

    08.09.24 - dropping tables

--- mySQL/Creating_Databases_and_Tables/index.sql
+++ mySQL/Creating_Databases_and_Tables/index.sql
@@ -30,8 +30,20 @@ CREATE TABLE dogs (
     age INT
 );
 
+-- Check tables
+
 SHOW tables;
 
 SHOW COLUMNS FROM cats;
 
 DESC cats;
+
+-- Drop tables
+
+To drop a table:
+
+DROP TABLE <table-name>;
+
+To specifically drop the cats table:
+
+DROP TABLE cats;
