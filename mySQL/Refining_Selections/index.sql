a39414623a93e33b1b7304bd3bbaa107ce740d52
Viktor <palchinskij19@gmail.com>
2024-09-09 18:16:57 +0300

    09.09.24 - more ORDER BY

--- mySQL/Refining_Selections/index.sql
+++ mySQL/Refining_Selections/index.sql
@@ -16,3 +16,9 @@ ORDER BY author_lname DESC;
  
 SELECT * FROM books
 ORDER BY released_year;
+
+SELECT book_id, author_fname, author_lname, pages
+FROM books ORDER BY 2 desc;
+ 
+SELECT book_id, author_fname, author_lname, pages
+FROM books ORDER BY author_lname, author_fname;
