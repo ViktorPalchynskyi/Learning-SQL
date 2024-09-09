6802637210a571d016959fa1d6998667ca845822
Viktor <palchinskij19@gmail.com>
2024-09-09 18:56:11 +0300

    09.09.24 -  COUNT;

--- mySQL/Refining_Selections/index.sql
+++ mySQL/Refining_Selections/index.sql
@@ -73,3 +73,11 @@ WHERE author_fname LIKE '____';
  
 SELECT * FROM books
 WHERE author_fname LIKE '_a_';
+
+-- To select books with '%' in their title:
+SELECT * FROM books
+WHERE title LIKE '%\%%';
+ 
+-- To select books with an underscore '_' in title:
+SELECT * FROM books
+WHERE title LIKE '%\_%';
