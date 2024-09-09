6800e49032f6cd87ff836373ee7d7343de678769
Viktor <palchinskij19@gmail.com>
2024-09-09 19:21:42 +0300

    09.09.24 -  Subqueries

--- mySQL/Aggregate_Functions/index.sql
+++ mySQL/Aggregate_Functions/index.sql
@@ -18,3 +18,11 @@ ORDER BY books_written DESC;
 SELECT MAX(pages) FROM books;
  
 SELECT MIN(author_lname) FROM books;
+
+SELECT title, pages FROM books
+WHERE pages = (SELECT MAX(pages) FROM books);
+ 
+SELECT MIN(released_year) FROM books;
+ 
+SELECT title, released_year FROM books 
+WHERE released_year = (SELECT MIN(released_year) FROM books);
