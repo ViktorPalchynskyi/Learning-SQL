cf4fbbc4535ec04848bf02765c383c46366a8b44
Viktor <palchinskij19@gmail.com>
2024-09-09 19:09:29 +0300

    09.09.24 -  GROUP BY

--- mySQL/Aggregate_Functions/index.sql
+++ mySQL/Aggregate_Functions/index.sql
@@ -3,3 +3,14 @@ SELECT COUNT(*) FROM books;
 SELECT COUNT(author_lname) FROM books;
  
 SELECT COUNT(DISTINCT author_lname) FROM books;
+
+SELECT author_lname, COUNT(*) 
+FROM books
+GROUP BY author_lname;
+ 
+SELECT 
+    author_lname, COUNT(*) AS books_written
+FROM
+    books
+GROUP BY author_lname
+ORDER BY books_written DESC;
