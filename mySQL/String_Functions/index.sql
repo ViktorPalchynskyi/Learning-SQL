01c52ab5c8203c7fbc148067ff3876fe1f0d1d8e
Viktor <palchinskij19@gmail.com>
2024-09-09 14:56:10 +0300

    09.09.24 - SUBSTRING function

--- mySQL/String_Functions/index.sql
+++ mySQL/String_Functions/index.sql
@@ -5,3 +5,13 @@ SELECT CONCAT('pi', 'ckle');
 SELECT CONCAT(author_fname,' ', author_lname) AS author_name FROM books;
  
 SELECT CONCAT_WS('-',title, author_fname, author_lname) FROM books;
+
+SELECT SUBSTRING('Hello World', 1, 4);
+ 
+SELECT SUBSTRING('Hello World', 7);
+ 
+SELECT SUBSTRING('Hello World', -3);
+ 
+SELECT SUBSTRING(title, 1, 10) AS 'short title' FROM books;
+ 
+SELECT SUBSTR(title, 1, 10) AS 'short title' FROM books;
