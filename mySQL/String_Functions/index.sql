7a01d811b97b32d66cd18de1560b146101af7447
Viktor <palchinskij19@gmail.com>
2024-09-09 15:15:54 +0300

    09.09.24 - CHAR_LENGTH & LENGTH function

--- mySQL/String_Functions/index.sql
+++ mySQL/String_Functions/index.sql
@@ -47,3 +47,11 @@ SELECT REVERSE(author_fname) FROM books;
 SELECT CONCAT('woof', REVERSE('woof'));
  
 SELECT CONCAT(author_fname, REVERSE(author_fname)) FROM books;
+
+SELECT CHAR_LENGTH('Hello World');
+ 
+SELECT CHAR_LENGTH(title) as length, title FROM books;
+ 
+SELECT author_lname, CHAR_LENGTH(author_lname) AS 'length' FROM books;
+ 
+SELECT CONCAT(author_lname, ' is ', CHAR_LENGTH(author_lname), ' characters long') FROM books;
