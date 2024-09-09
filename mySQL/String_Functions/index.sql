9fe695d96cd3ca3b5088c34465812ce9a7536a3f
Viktor <palchinskij19@gmail.com>
2024-09-09 15:10:33 +0300

    09.09.24 - REVERSE function

--- mySQL/String_Functions/index.sql
+++ mySQL/String_Functions/index.sql
@@ -37,3 +37,13 @@ SELECT
 SELECT REPLACE(title, 'e ', '3') FROM books;
  
 SELECT REPLACE(title, ' ', '-') FROM books;
+
+SELECT REVERSE('Hello World');
+ 
+SELECT REVERSE('meow meow');
+ 
+SELECT REVERSE(author_fname) FROM books;
+ 
+SELECT CONCAT('woof', REVERSE('woof'));
+ 
+SELECT CONCAT(author_fname, REVERSE(author_fname)) FROM books;
