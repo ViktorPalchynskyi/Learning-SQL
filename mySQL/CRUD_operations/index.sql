367ea31eec934603114050e6017ad38a3578330d
Viktor <palchinskij19@gmail.com>
2024-09-09 13:22:49 +0300

    09.09.24 - WHERE operator

--- mySQL/CRUD_operations/index.sql
+++ mySQL/CRUD_operations/index.sql
@@ -9,3 +9,9 @@ SELECT age FROM cats;
 -- To select multiple specific columns:
 
 SELECT name, breed FROM cats;
+
+-- Use where to specify a condition:
+
+SELECT * FROM cats WHERE age = 4;
+
+SELECT * FROM cats WHERE name ='Egg';
