f7a856edd9083754513a30cea0f097f23f2228c9
Viktor <palchinskij19@gmail.com>
2024-09-09 15:06:52 +0300

    09.09.24 - REPLACE function

--- mySQL/String_Functions/index.sql
+++ mySQL/String_Functions/index.sql
@@ -15,3 +15,25 @@ SELECT SUBSTRING('Hello World', -3);
 SELECT SUBSTRING(title, 1, 10) AS 'short title' FROM books;
  
 SELECT SUBSTR(title, 1, 10) AS 'short title' FROM books;
+
+SELECT CONCAT
+    (
+        SUBSTRING(title, 1, 10),
+        '...'
+    ) AS 'short title'
+FROM books;
+
+SELECT REPLACE('Hello World', 'Hell', '%$#@');
+ 
+SELECT REPLACE('Hello World', 'l', '7');
+ 
+SELECT REPLACE('Hello World', 'o', '0');
+ 
+SELECT REPLACE('HellO World', 'o', '*');
+ 
+SELECT
+  REPLACE('cheese bread coffee milk', ' ', ' and ');
+ 
+SELECT REPLACE(title, 'e ', '3') FROM books;
+ 
+SELECT REPLACE(title, ' ', '-') FROM books;
