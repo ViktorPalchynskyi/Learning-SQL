fc5fc713176e59b903c4a38ee88a3e82531cb534
Viktor <palchinskij19@gmail.com>
2024-09-09 18:10:28 +0300

    09.09.24 - ORDER BY

--- mySQL/Refining_Selections/index.sql
+++ mySQL/Refining_Selections/index.sql
@@ -7,3 +7,12 @@ SELECT author_fname, author_lname FROM books;
 SELECT DISTINCT CONCAT(author_fname,' ', author_lname) FROM books;
  
 SELECT DISTINCT author_fname, author_lname FROM books;
+
+SELECT * FROM books
+ORDER BY author_lname;
+ 
+SELECT * FROM books
+ORDER BY author_lname DESC;
+ 
+SELECT * FROM books
+ORDER BY released_year;
