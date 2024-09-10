3a324046fa306ecc07476d6e630d085940851f5d
Viktor <palchinskij19@gmail.com>
2024-09-10 19:02:39 +0300

    10.09.24 - LEFT JOIN with GROUP BY and IFNULL function

--- mySQL/One_to_Many_and_Joins/index.sql
+++ mySQL/One_to_Many_and_Joins/index.sql
@@ -92,3 +92,13 @@ FROM
     orders
         LEFT JOIN
     customers ON orders.customer_id = customers.id;
+
+SELECT 
+    first_name, 
+    last_name, 
+    IFNULL(SUM(amount), 0) AS money_spent
+FROM
+    customers
+        LEFT JOIN
+    orders ON customers.id = orders.customer_id
+GROUP BY first_name , last_name;
