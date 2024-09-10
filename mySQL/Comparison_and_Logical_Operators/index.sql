06fe2e45a43f9a45ce96bb0a5581dc72bec19949
Viktor <palchinskij19@gmail.com>
2024-09-10 13:49:36 +0300

    10.09.24 - <= operator

--- mySQL/Comparison_and_Logical_Operators/index.sql
+++ mySQL/Comparison_and_Logical_Operators/index.sql
@@ -9,3 +9,12 @@ WHERE released_year > 2005;
  
 SELECT * FROM books
 WHERE pages > 500;
+
+SELECT * FROM books
+WHERE pages < 200;
+ 
+SELECT * FROM books
+WHERE released_year < 2000;
+ 
+SELECT * FROM books
+WHERE released_year <= 1985;
