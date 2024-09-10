9e846d8681a98ec7883ca03d84a86680b6264c30
Viktor <palchinskij19@gmail.com>
2024-09-10 14:09:37 +0300

    10.09.24 - Compare dates

--- mySQL/Comparison_and_Logical_Operators/index.sql
+++ mySQL/Comparison_and_Logical_Operators/index.sql
@@ -52,3 +52,11 @@ AND released_year >= 2004;
  
 SELECT title, released_year FROM books
 WHERE released_year BETWEEN 2004 AND 2014;
+
+SELECT * FROM people WHERE birthtime 
+BETWEEN CAST('12:00:00' AS TIME) 
+AND CAST('16:00:00' AS TIME);
+ 
+ 
+SELECT * FROM people WHERE HOUR(birthtime)
+BETWEEN 12 AND 16;
