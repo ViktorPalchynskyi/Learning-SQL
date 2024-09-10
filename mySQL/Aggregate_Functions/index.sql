4d73be50f429ec957481868d06325064cd71c933
Viktor <palchinskij19@gmail.com>
2024-09-10 12:17:54 +0300

    10.09.24 - More date functions

--- mySQL/Aggregate_Functions/index.sql
+++ mySQL/Aggregate_Functions/index.sql
@@ -97,3 +97,16 @@ SELECT NOW();
  
 INSERT INTO people (name, birthdate, birthtime, birthdt)
 VALUES ('Hazel', CURDATE(), CURTIME(), NOW());
+
+SELECT 
+    birthdate,
+    DAY(birthdate),
+    DAYOFWEEK(birthdate),
+    DAYOFYEAR(birthdate)
+FROM people;
+ 
+SELECT 
+    birthdate,
+    MONTHNAME(birthdate),
+    YEAR(birthdate)
+FROM people;
