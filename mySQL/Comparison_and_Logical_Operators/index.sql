ac644ef3c4242b16c7c4c8e8ced47dc8e8f30938
Viktor <palchinskij19@gmail.com>
2024-09-10 14:15:25 +0300

    10.09.24 - IN, NOT IN operators

--- mySQL/Comparison_and_Logical_Operators/index.sql
+++ mySQL/Comparison_and_Logical_Operators/index.sql
@@ -60,3 +60,19 @@ AND CAST('16:00:00' AS TIME);
  
 SELECT * FROM people WHERE HOUR(birthtime)
 BETWEEN 12 AND 16;
+
+SELECT title, author_lname FROM books
+WHERE author_lname = 'Carver' 
+OR author_lname = 'Lahiri'
+OR author_lname = 'Smith';
+ 
+SELECT title, author_lname FROM books
+WHERE author_lname IN ('Carver', 'Lahiri', 'Smith');
+ 
+SELECT title, author_lname FROM books
+WHERE author_lname NOT IN ('Carver', 'Lahiri', 'Smith');
+ 
+ 
+SELECT title, released_year FROM books
+WHERE released_year >= 2000 
+AND released_year % 2 = 1;
