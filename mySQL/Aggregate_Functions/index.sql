99fa8a74b3c435ec0932aced21d4a817fdd74be0
Viktor <palchinskij19@gmail.com>
2024-09-10 12:20:33 +0300

    10.09.24 - time functions

--- mySQL/Aggregate_Functions/index.sql
+++ mySQL/Aggregate_Functions/index.sql
@@ -110,3 +110,17 @@ SELECT
     MONTHNAME(birthdate),
     YEAR(birthdate)
 FROM people;
+
+SELECT 
+    birthtime,
+    HOUR(birthtime),
+    MINUTE(birthtime)
+FROM people;
+ 
+SELECT 
+    birthdt,
+    MONTH(birthdt),
+    DAY(birthdt),
+    HOUR(birthdt),
+    MINUTE(birthdt)
+FROM people;
