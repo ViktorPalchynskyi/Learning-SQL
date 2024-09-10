b00340217e42ffa97e2045e39c9dbd7ad3eba546
Viktor <palchinskij19@gmail.com>
2024-09-10 15:15:13 +0300

    10.09.24 - CHECK constrains

--- mySQL/Constraints_and_ALTER_TABLE/index.sql
+++ mySQL/Constraints_and_ALTER_TABLE/index.sql
@@ -9,3 +9,12 @@ VALUES ('billybob', '8781213455');
 -- This insert would result in an error:
 INSERT INTO contacts (name, phone)
 VALUES ('billybob', '8781213455');
+
+CREATE TABLE users (
+	username VARCHAR(20) NOT NULL,
+    age INT CHECK (age > 0)
+);
+ 
+CREATE TABLE palindromes (
+  word VARCHAR(100) CHECK(REVERSE(word) = word)
+)
