6c8645975ec8b8820e7c69ef4ce19dd1cc4a6e73
Viktor <palchinskij19@gmail.com>
2024-09-10 18:58:35 +0300

    10.09.24 - LEFT JOIN

--- mySQL/One_to_Many_and_Joins/index.sql
+++ mySQL/One_to_Many_and_Joins/index.sql
@@ -77,3 +77,18 @@ FROM
     orders ON orders.customer_id = customers.id
 GROUP BY first_name , last_name
 ORDER BY total;
+
+SELECT 
+    first_name, last_name, order_date, amount
+FROM
+    customers
+        LEFT JOIN
+    orders ON orders.customer_id = customers.id;
+ 
+ 
+SELECT 
+    order_date, amount, first_name, last_name
+FROM
+    orders
+        LEFT JOIN
+    customers ON orders.customer_id = customers.id;
