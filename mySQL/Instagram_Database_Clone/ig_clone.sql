70fe32271f9c919eec2bf8aad06ad572d9994839
Viktor <palchinskij19@gmail.com>
2024-09-11 13:31:06 +0300

    11.09.24 - User Schema;

---
+++ mySQL/Instagram_Database_Clone/ig_clone.sql
@@ -0,0 +1,7 @@
+CREATE TABLE users(
+	id INT PRIMARY KEY AUTO_INCREMENT, 
+	username VARCHAR(255) UNIQUE NOT NULL,
+	created_at TIMESTAMP DEFAULT NOW()
+);
+
+INSERT INTO users (username) VALUES ('Viktor'), ('Irina');
