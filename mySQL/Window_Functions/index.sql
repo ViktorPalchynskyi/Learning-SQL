17f12b325b1f94d5b8d8e6ba620955af2c6749c9
Viktor <palchinskij19@gmail.com>
2024-09-11 13:05:31 +0300

    11.09.24 - FIRST_VALUE()

--- mySQL/Window_Functions/index.sql
+++ mySQL/Window_Functions/index.sql
@@ -100,3 +100,11 @@ SELECT
     NTILE(4) OVER(PARTITION BY department ORDER BY salary DESC) AS dept_salary_quartile,
 	NTILE(4) OVER(ORDER BY salary DESC) AS salary_quartile
 FROM employees;
+
+SELECT 
+    emp_no, 
+    department, 
+    salary,
+    FIRST_VALUE(emp_no) OVER(PARTITION BY department ORDER BY salary DESC) as highest_paid_dept,
+    FIRST_VALUE(emp_no) OVER(ORDER BY salary DESC) as highest_paid_overall
+FROM employees;
