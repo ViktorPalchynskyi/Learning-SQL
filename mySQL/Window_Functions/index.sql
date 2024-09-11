75c4a35af70e860382db5dd3d803f7036620555e
Viktor <palchinskij19@gmail.com>
2024-09-11 12:27:29 +0300

    11.09.24 - ORDER BY with windows

--- mySQL/Window_Functions/index.sql
+++ mySQL/Window_Functions/index.sql
@@ -66,3 +66,18 @@ SELECT
     SUM(salary) OVER(PARTITION BY department) AS dept_payroll,
     SUM(salary) OVER() AS total_payroll
 FROM employees;
+
+SELECT 
+    emp_no, 
+    department, 
+    salary, 
+    SUM(salary) OVER(PARTITION BY department ORDER BY salary) AS rolling_dept_salary,
+    SUM(salary) OVER(PARTITION BY department) AS total_dept_salary
+FROM employees;
+ 
+SELECT 
+    emp_no, 
+    department, 
+    salary, 
+    MIN(salary) OVER(PARTITION BY department ORDER BY salary DESC) as rolling_min
+FROM employees;
