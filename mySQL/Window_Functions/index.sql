46e1fff796e7a3cbf49c75298017a7176e9363a6
Viktor <palchinskij19@gmail.com>
2024-09-11 13:00:14 +0300

    11.09.24 - NTILE()

--- mySQL/Window_Functions/index.sql
+++ mySQL/Window_Functions/index.sql
@@ -92,3 +92,11 @@ SELECT
     DENSE_RANK() OVER(ORDER BY salary DESC) as overall_dense_rank,
     ROW_NUMBER() OVER(ORDER BY salary DESC) as overall_num
 FROM employees ORDER BY overall_rank;
+
+SELECT 
+    emp_no, 
+    department, 
+    salary,
+    NTILE(4) OVER(PARTITION BY department ORDER BY salary DESC) AS dept_salary_quartile,
+	NTILE(4) OVER(ORDER BY salary DESC) AS salary_quartile
+FROM employees;
